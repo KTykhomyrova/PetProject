df0833b778641cd0ba6254f3fd89333a6840106d
s.chepets <s.chepets@plarium.com>
2025-07-19 12:08:23 +0200

    Refactor UI layout and update ViewModel properties
    - Removed unnecessary using directives in `Feedback.cs`.
    - Changed layout in `MainWindow.xaml` from `StackPanel` to `Grid` for better structure.
    - Updated `Button` command from `SaveCommand` to `CreateCommand`.
    - Modified `ListBox` item template to use a `Grid` for feedback items.
    - Introduced `[ObservableProperty]` attribute in `MainWindowViewModel.cs` for cleaner property change notifications.
    - Changed default `Rating` value from 10 to 5.
    - Renamed `Save` method to `Create` and added a placeholder `Update` method.
    - Added `FillDesignDataIfNeed` method for design-time data population.
    
    (#2)

--- FeedbackHub/FeedbackHub/Model/Entities/Feedback.cs
+++ FeedbackHub/FeedbackHub/Model/Entities/Feedback.cs
@@ -1,9 +1,4 @@
 ﻿using FeedbackHub.Model.Contracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FeedbackHub.Model.Entities
 {
